2a57c25ab5088c079acee7e88aa139439ab29dfd
adityaparvatepatil <aparvatepatil@gmail.com>
2022-10-24 14:05:08 -0400

    Add medicine

---
+++ medicinepage/script.js
@@ -0,0 +1,87 @@
+function funcOpen(whatButton) {
+    
+    var inputButton = document.getElementsByClassName("Button Name");
+
+    for (var i = 0; i < inputButton.length; i++) {
+      inputButton[i].style.display = "none";  
+    
+    }
+    document.getElementById(whatButton).style.display = "block";  
+}
+
+
+  // Function for dropdown
+  function dropSelect() {
+      var inputOption  = document.getElementById("dropdownID");
+      var display = inputOption.options[inputOption.selectedIndex].text;
+      if (display.length == 3 || display.length == 4) {
+          document.getElementById("txtValue").value = "Player 2 guesses their hat color is " + display ;
+      }
+      else {
+          document.getElementById("txtValue").value = ""
+      }
+  }
+  function dropRadio() {
+      var color = document.querySelector('input[name = "groupColor"]:checked').value;
+      document.getElementById("colorOption").value = "Player 1 guesses their hat color is " + color;
+  }
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_todo 
+
+    var lst = document.getElementsByTagName("LI");
+
+    for (var i = 0; i < lst.length; i++) {
+        var string = document.createElement("SPAN");
+        var txt = document.createTextNode("\u00D7");
+        string.className = "close";
+        string.appendChild(txt);
+        lst[i].appendChild(string);
+    }
+
+    var close = document.getElementsByClassName("close");
+
+    for (var i = 0; i < close.length; i++) {
+        close[i].onclick = function() {
+          this.parentElement.style.display = "none";
+        }
+    }
+
+    var lst = document.querySelector('ul');
+
+    lst.addEventListener('click', function(inputName) {
+      if (inputName.target.tagName === 'LI') {
+        inputName.target.classList.toggle('checked');
+      }
+    }
+    , false);
+
+
+    function newElement() {
+        var li = document.createElement("li");
+        var inputValue = document.getElementById("myInput").value;
+        var t = document.createTextNode(inputValue);
+        li.appendChild(t);
+
+        if (inputValue === '') {
+          alert("Item cannot be empty!");
+        }
+        else {
+          document.getElementById("myUL").appendChild(li);
+        }
+        
+        document.getElementById("myInput").value = "";
+        var span = document.createElement("SPAN");
+        var txt = document.createTextNode("\u00D7");
+        span.className = "close";
+        span.appendChild(txt);
+        li.appendChild(span);
+
+        for (i = 0; i < close.length; i++) {
+          close[i].onclick = function() {
+            this.parentElement.style.display = "none";
+          }
+        }
+    }
+    
+  
