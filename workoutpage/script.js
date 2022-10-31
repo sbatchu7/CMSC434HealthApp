3ed8621d8fac14162859cc878b41bf891956e07c
Sumanth Batchu <sbatchu1@terpmail.umd.edu>
2022-10-31 16:42:28 -0400

    update

--- workoutpage/script.js
+++ workoutpage/script.js
@@ -50,6 +50,136 @@ document.querySelector('#pushTue').onclick = function () {
     }
 }
 
+document.querySelector('#pushWed').onclick = function () {
+    if (document.querySelector('#newtaskWed input').value.length == 0) {
+        alert("Please enter a Workout and the number of reps")
+    }
+
+    else {
+        document.querySelector('#tasksWed').innerHTML += `
+            <div class="taskWed">
+                <span id="tasknameWed">
+                    ${document.querySelector('#newtaskWed input').value}
+                </span>
+                <button class="deleteWed">
+                    <i class="far fa-trash-alt"></i>
+                </button>
+            </div>
+        `;
+
+        var current_tasks = document.querySelectorAll(".deleteWed");
+        for (var i = 0; i < current_tasks.length; i++) {
+            current_tasks[i].onclick = function () {
+                this.parentNode.remove();
+            }
+        }
+    }
+}
+
+document.querySelector('#pushThu').onclick = function () {
+    if (document.querySelector('#newtaskThu input').value.length == 0) {
+        alert("Please enter a Workout and the number of reps")
+    }
+
+    else {
+        document.querySelector('#tasksThu').innerHTML += `
+            <div class="taskThu">
+                <span id="tasknameThu">
+                    ${document.querySelector('#newtaskThu input').value}
+                </span>
+                <button class="deleteThu">
+                    <i class="far fa-trash-alt"></i>
+                </button>
+            </div>
+        `;
+
+        var current_tasks = document.querySelectorAll(".deleteThu");
+        for (var i = 0; i < current_tasks.length; i++) {
+            current_tasks[i].onclick = function () {
+                this.parentNode.remove();
+            }
+        }
+    }
+}
+
+document.querySelector('#pushFri').onclick = function () {
+    if (document.querySelector('#newtaskFri input').value.length == 0) {
+        alert("Please enter a Workout and the number of reps")
+    }
+
+    else {
+        document.querySelector('#tasksFri').innerHTML += `
+            <div class="taskFri">
+                <span id="tasknameFri">
+                    ${document.querySelector('#newtaskFri input').value}
+                </span>
+                <button class="deleteFri">
+                    <i class="far fa-trash-alt"></i>
+                </button>
+            </div>
+        `;
+
+        var current_tasks = document.querySelectorAll(".deleteFri");
+        for (var i = 0; i < current_tasks.length; i++) {
+            current_tasks[i].onclick = function () {
+                this.parentNode.remove();
+            }
+        }
+    }
+}
+
+document.querySelector('#pushSat').onclick = function () {
+    if (document.querySelector('#newtaskSat input').value.length == 0) {
+        alert("Please enter a Workout and the number of reps")
+    }
+
+    else {
+        document.querySelector('#tasksSat').innerHTML += `
+            <div class="taskSat">
+                <span id="tasknameSat">
+                    ${document.querySelector('#newtaskSat input').value}
+                </span>
+                <button class="deleteSat">
+                    <i class="far fa-trash-alt"></i>
+                </button>
+            </div>
+        `;
+
+        var current_tasks = document.querySelectorAll(".deleteSat");
+        for (var i = 0; i < current_tasks.length; i++) {
+            current_tasks[i].onclick = function () {
+                this.parentNode.remove();
+            }
+        }
+    }
+}
+
+document.querySelector('#pushSun').onclick = function () {
+    if (document.querySelector('#newtaskSun input').value.length == 0) {
+        alert("Please enter a Workout and the number of reps")
+    }
+
+    else {
+        document.querySelector('#tasksSun').innerHTML += `
+            <div class="taskSun">
+                <span id="tasknameSun">
+                    ${document.querySelector('#newtaskSun input').value}
+                </span>
+                <button class="deleteSun">
+                    <i class="far fa-trash-alt"></i>
+                </button>
+            </div>
+        `;
+
+        var current_tasks = document.querySelectorAll(".deleteSun");
+        for (var i = 0; i < current_tasks.length; i++) {
+            current_tasks[i].onclick = function () {
+                this.parentNode.remove();
+            }
+        }
+    }
+}
+
 function funcOpen(whatButton) {
 
     var inputButton = document.getElementsByClassName("Button Name");
