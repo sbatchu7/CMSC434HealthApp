53ba86711cb070cb1cd1a7a067f4462cc8336d55
adityaparvatepatil <aparvatepatil@gmail.com>
2022-11-01 16:43:26 -0400

    Medicine update

---
+++ medicinepage/keyboard.js
@@ -0,0 +1,10 @@
+function searchBoxClicked() {
+  console.log('focus');
+  var yourImg = document.getElementById('yourImgId');
+  yourImg.style.height = '30%';
+}
+
+function closeboard() {
+  var yourImg = document.getElementById('yourImgId');
+  yourImg.style.height = '0%';
+}
--- medicinepage/script.js
+++ medicinepage/script.js
@@ -85,3 +85,13 @@ function funcOpen(whatButton) {
     }
     
   
+    function searchBoxClicked() {
+      console.log('focus');
+      var yourImg = document.getElementById('yourImgId');
+      yourImg.style.height = '30%';
+    }
+    
+    function closeboard() {
+      var yourImg = document.getElementById('yourImgId');
+      yourImg.style.height = '0%';
+    }
